252da61cfe2566ef8974f50f5280b3225d3e4ebf
Gabriel Luiz Cardoso Bobello <bobello.gabriel@gmail.com>
2022-04-15 13:50:04 -0300

    Enviando Outros exercícios do Uri/becrow

--- src/UriAreaCirculo.java
+++ src/UriAreaCirculo.java
@@ -3,6 +3,7 @@ public class UriAreaCirculo {
     public static void main(String[] args){
         Scanner teclado = new Scanner(System.in);
         double raio = teclado.nextDouble();
+        float a, b, c;
         double area = 3.14159 * (raio*raio);
         System.out.println(String.format("A=%.4f", area));
     }
